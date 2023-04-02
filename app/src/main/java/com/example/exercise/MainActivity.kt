66bac1676c77ea474fdfb6d13bac23379eb7c80c
ivanpahlevi8 <83549388+ivanpahlevi8@users.noreply.github.com>
2023-04-02 13:04:17 +0700

    Second Commit

--- app/src/main/java/com/example/exercise/MainActivity.kt
+++ app/src/main/java/com/example/exercise/MainActivity.kt
@@ -3,14 +3,28 @@ package com.example.exercise
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity(), View. OnClickListener{
+
+    // get button
+    var showBlBtn : Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showBlBtn = findViewById(R.id.showBl)
+
+        showBlBtn?.setOnClickListener(this)
     }
 
     override fun onClick(p0: View?) {
-        TODO("Not yet implemented")
+        when(p0?.id) {
+            R.id.showBl -> {
+                // when button get pressed
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/exercise/dialog/ShowBl.kt
+++ app/src/main/java/com/example/exercise/dialog/ShowBl.kt
@@ -1,12 +1,28 @@
 package com.example.exercise.dialog
 
+import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.exercise.R
 
 class ShowBl : DialogFragment() {
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return super.onCreateDialog(savedInstanceState)
 
+    // get item from xml file
+    var reView : RecyclerView? = null
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(this.requireActivity())
+        val myInfalter = this.requireActivity().layoutInflater
+        val myView = myInfalter.inflate(R.layout.show_bl, null)
+
+        reView = myView.findViewById(R.id.recyclerView)
+
+
+
+        builder.setView(myView)
+
+        return builder.show()
     }
 }
